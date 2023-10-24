502465c4f55f624c99c95b01c0036db3bb2ef271
Amila Madusanka <amila.madusanka@xitricon.com>
2023-10-24 19:12:50 +0530

    fix an security hotspot

--- src/main/java/com/xitricon/workflowservice/config/WebConfig.java
+++ src/main/java/com/xitricon/workflowservice/config/WebConfig.java
@@ -2,7 +2,6 @@ package com.xitricon.workflowservice.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,11 +17,6 @@ public class WebConfig implements WebMvcConfigurer {
 		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
 
 		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-	}
-
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
 	}
 
 }
