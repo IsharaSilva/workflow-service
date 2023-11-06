eb0f9d088535292b26d70aec0a16eeaeb128a826
kasuni <kasuni.samarasekara@xitricon.com>
2023-11-06 09:33:21 +0530

    added tests

---
+++ src/test/java/com/xitricon/workflowservice/util/WorkFlowUtilTest.java
@@ -0,0 +1,30 @@
+package com.xitricon.workflowservice.util;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class WorkFlowUtilTest {
+
+	@Test
+	public void testPrivateConstructorInvocation() throws Exception {
+		final Constructor<WorkflowUtil> constructor = WorkflowUtil.class.getDeclaredConstructor();
+
+		// check that all constructors are 'private':
+		assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+
+		constructor.setAccessible(true);
+		try {
+			constructor.newInstance();
+			Assertions.fail("Constructor is not a privtae one");
+		} catch (InvocationTargetException e) {
+			assertTrue(e.getTargetException() instanceof IllegalStateException);
+		}
+	}
+
+}
